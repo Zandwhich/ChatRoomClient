26ca5cbff7071c5741ebb125abacb087480c6ea5
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2020-04-04 22:47:58 -0700

    Created skeleton classes for some of the view aspects

---
+++ src/view/SendButton.java
@@ -0,0 +1,13 @@
+/*
+ * Author: Alex Zdanowicz
+ */
+
+package view;
+
+import javax.swing.*;
+
+/**
+ * The "Send" button in the bottom left-hand corner
+ */
+public class SendButton extends JButton {
+}//end SendButton
